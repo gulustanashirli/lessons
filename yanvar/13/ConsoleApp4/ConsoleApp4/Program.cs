07fd1eec71f909e75e9e487230fe4d8f5432f6e1
gulustan ashirova <ashirovaashirova160@gmail.com>
2023-01-19 23:04:25 +0400

    My homework3 is ready

---
+++ yanvar/13/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("10 seher adi daxil edin ");
+            string City1 = "seher1";
+            Console.WriteLine(City1);
+            string Cityname1 = Console.ReadLine();
+            string City2 = "seher2";
+            Console.WriteLine(City2);
+            string Cityname2 = Console.ReadLine();
+            string City3 = "seher3 ";
+            Console.WriteLine(City3);
+            string Cityname3 = Console.ReadLine();
+            string City4 = "Seher4";
+            Console.WriteLine(City4);
+            string Cityname4 = Console.ReadLine();
+            string City5 = "seher5";
+            Console.WriteLine(City5);
+            string Cityname5 = Console.ReadLine();
+            string City6 = "seher6";
+            Console.WriteLine(City6);
+            string Cityname6 = Console.ReadLine();
+            string City7 = "Seher7";
+            Console.WriteLine(City7);
+            string Cityname7 = Console.ReadLine();
+            string City8 = "Seher8";
+            Console.WriteLine(City8);
+            string Cityname8 = Console.ReadLine();
+            string City9 = "seher9";
+            Console.WriteLine(City9);
+            string Cityname9 = Console.ReadLine();
+            string City10 = " Seher10";
+            Console.WriteLine(City10);
+            string Cityname10 = Console.ReadLine();
+            Console.ReadLine();
+       
+
+
+
+
+            Console.WriteLine("10 seher adi daxil edin :");
+            Console.Write("Baki,");
+            Console.Write("Milan,");
+            Console.Write("Roma,");
+            Console.Write("Madrid,");
+            Console.Write("Ankara,");
+            Console.Write("Istanbul,");
+            Console.Write("Kopenhagen,");
+            Console.Write("Helsinki,");
+            Console.Write("Los Angeles,");
+            Console.Write("San Fransisko");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
