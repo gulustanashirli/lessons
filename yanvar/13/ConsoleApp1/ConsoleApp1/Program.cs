88b7fc2f64a6f0620833237aeb95312801c035ee
gulustan ashirova <ashirovaashirova160@gmail.com>
2023-01-19 22:55:29 +0400

    My homework is ready

---
+++ yanvar/13/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Homework1
+            string whatIsYourName = "Adiniz nedir?";
+            Console.WriteLine(whatIsYourName);
+            string name = Console.ReadLine();
+            string whatIsYourSurname = "Soy adiniz nedir?";
+            Console.WriteLine(whatIsYourSurname);
+            string Surname = Console.ReadLine();
+            string whereDoYouLive = "Harada Yasayirsiniz?";
+            Console.WriteLine(whereDoYouLive);
+            string YouLive = Console.ReadLine();
+            Console.ReadLine();
+            #endregion
+
+        }
+    }
+}
